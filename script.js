d74f000152d9e53067cd6e742d69c79187bb8db9
lrwthns <lrwthns@gmail.com>
2021-03-08 01:15:42 +0700

    change submit method and add comments

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let newBookButton = document.querySelector('#newBook');
 let displayContainer = document.querySelector('#display-container');
 let form = document.querySelector('#form');
-let submitForm = document.querySelector('#submit');
 let deleteBook = document.querySelector('#deleteBook');
 let myLibrary = [];
 
@@ -24,46 +23,59 @@ function Book(title, author, numOfPages, readStatus) {
 
 //prints info that will be put on the book display
 Book.prototype.info = function() {
-   return this.title+ ' by ' + this.author + ', ' + this.numOfPages + ' pages, ' + this.readStatus;
+   return `${this.title} by ${this.author} ${this.numOfPages} pages`;
 }
 
-//add the book object into the myLibrary array
+//adds the book object into the myLibrary array
 function addBookToLibrary(book) {
     let bookToBeAdded = book;
     return myLibrary.push(bookToBeAdded);
 }
 
-//display myLibrary array
+//sets initial read status
+function toggleReadStatusText (prop, textbox) {
+    if (prop == false) {
+        textbox.innerHTML = 'Not Read';
+    } else {
+        textbox.innerHTML = 'Read';
+    }
+}
+
+//displays all the book object in myLibrary 
 function displayBook(arr) {
+    //this reset the display container so there won't be multiple displays of the same book when myLibrary is updated
     while (displayContainer.firstChild) {
         displayContainer.removeChild(displayContainer.firstChild);
     }
+    //for each book object in the myLibrary array..
     for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
+        //create new elements in a display
         let newBookDisplayDiv = document.createElement('div');
+        let textArea = document.createElement('div');
         let deleteButton = document.createElement('button');
         let toggleReadStatus = document.createElement('button');
-        let txt = '';
+        //add class for each new element
         newBookDisplayDiv.classList.add('bookDisplay');
+        textArea.classList.add('displayText');
         deleteButton.classList.add('deleteBook');
         toggleReadStatus.classList.add('readStatus');
+        //append elements to their parents
         displayContainer.appendChild(newBookDisplayDiv);
-        txt += arr[i].info();
-        newBookDisplayDiv.innerHTML = txt;
+        newBookDisplayDiv.appendChild(textArea);
+        textArea.innerHTML += arr[i].info();
         newBookDisplayDiv.appendChild(deleteButton);
         newBookDisplayDiv.appendChild(toggleReadStatus);
         deleteButton.innerHTML = 'Delete Book';
-        toggleReadStatus.innerHTML = 'Not Read';
         deleteButton.addEventListener('click', () => {
             arr.splice(i, 1);
             populateStorage(myLibrary);
             return displayBook(myLibrary);
-        })
+        })    
+        toggleReadStatusText(arr[i].readStatus, toggleReadStatus);
         toggleReadStatus.addEventListener('click', () => {
             if (toggleReadStatus.innerHTML == 'Not Read') {
                 return toggleReadStatus.innerHTML = 'Read';
-            }
-            return toggleReadStatus.innerHTML = 'Not Read';
+            } return toggleReadStatus.innerHTML = 'Not Read';
         })
     }
 }
@@ -81,11 +93,11 @@ function checkLocalStorage() {
         for (let i = 0; i < retrievedLibrary.length; i++) {
             let newBookObject = new Book(retrievedLibrary[i].title, retrievedLibrary[i].author, retrievedLibrary[i].numOfPages, retrievedLibrary[i].readStatus);
             addBookToLibrary(newBookObject);
-        }
-        return displayBook(myLibrary);
+        } return displayBook(myLibrary);
     }
 }
 
+//this resets the form
 function resetForm() {
     document.querySelector('#bookTitle').value = '';
     document.querySelector('#bookAuthor').value = '';
@@ -94,8 +106,9 @@ function resetForm() {
 }
 
 //where input is processed after user clicks submit
-submitForm.addEventListener('click', (ev) => {
-    ev.preventDefault();
+form.addEventListener('submit', (event) => {
+    //stop the default process of submitting the form, replacing the action with our own functions, without this the display would only flash briefly before it's gone
+    event.preventDefault();
     let bookTitle = document.querySelector('#bookTitle').value;
     let bookAuthor = document.querySelector('#bookAuthor').value;
     let bookPages = document.querySelector('#bookPages').value;
@@ -103,10 +116,9 @@ submitForm.addEventListener('click', (ev) => {
     let checkCheckbox = () => {
         let checkbox = document.querySelector('#bookHaveFinished');
             if (checkbox.checked == true) {
-                return 'is finished.'
-            } else {
-                return 'is not finished.'
+                return true;
             }
+            return false;
         };
     let bookHaveFinished = checkCheckbox();
     //creates a new book object instance
@@ -118,3 +130,6 @@ submitForm.addEventListener('click', (ev) => {
     newBookButton.style.display = 'block';
     resetForm();
 })
+
+//calls the function that will return saved MyLibrary array if there's one
+checkLocalStorage();
